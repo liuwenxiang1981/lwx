0bc62d5ba77f216097ddee63706c4135afd92285
liuwenxiang1981 <41276428+liuwenxiang1981@users.noreply.github.com>
2019-01-11 19:47:37 -0800

    Add files via upload

---
+++ 0112.py
@@ -0,0 +1,11 @@
+# import requests
+# response = requests.get("http://www.baidu.com")
+# print(response.text)
+# print(response.headers)
+# print(response.status_code)
+# # 也可以加入请求头的方式,去伪装成浏览器
+# headers = {'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+# response = requests.get('http://www.baidu.com',headers=headers)
+# print(response.text)
+
+
